1504829c9bd2395034b236e7fd2cb86d4274fd9b
GuiSteinn <guilhermezuninosgrott@gmail.com>
2025-03-05 19:21:45 -0300

    ok

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
 
-    require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-    $mpdf = new \Mpdf\Mpdf();
-    $mpdf ->WriteHTML('<h1>Hello,</h1>');
+// Criar uma instância do mPDF
+$mpdf = new \Mpdf\Mpdf();
 
-    $mpdf -> Output();
+$html = '
+    <h1>Hello,</h1>
+    <p>Este é um exemplo de PDF gerado com a biblioteca mPDF.</p>
+    <p>Aqui está uma foto do Messi:</p>
+    <img src="https://assets.goal.com/images/v3/bltd58c4d60ecd9275e/GOAL_-_Blank_WEB_-_Facebook_-_2023-06-13T135350.847.png?auto=webp&format=pjpg&width=1080&quality=60" alt="Lionel Messi" style="width: 300px;">
+';
 
+// Adicionar o conteúdo HTML ao PDF
+$mpdf->WriteHTML($html);
 
+// Saída do PDF para o navegador
+$mpdf->Output();
 
 ?>
